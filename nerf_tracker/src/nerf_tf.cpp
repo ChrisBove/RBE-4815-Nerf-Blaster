7e5da066fd345077a009e0620f568b60cd6269c8
ChrisBove <bove.3123@gmail.com>
2016-04-27 13:41:27 -0400

    fixed cntr c not working when searching for tf

--- nerf_tracker/src/nerf_tf.cpp
+++ nerf_tracker/src/nerf_tf.cpp
@@ -43,26 +43,30 @@ void NerfTF::lookupTransform(){
 	std::stringstream ss;
 	ss << "/torso_" << activeUser;
 
-	try {
-		listener.lookupTransform("/openni_depth_frame", ss.str(), ros::Time(0),
-				transform);
+	if (ros::ok()) {
+		try {
+			listener.lookupTransform("/openni_depth_frame", ss.str(),
+					ros::Time(0), transform);
 
-		// right of kinect is negative yaw
-		yaw = atan2(transform.getOrigin().y(),
+			// right of kinect is negative yaw
+			yaw = atan2(transform.getOrigin().y(), transform.getOrigin().x());
+			// above kinect is negative pitch
+			pitch = -atan2(transform.getOrigin().z(),
 					transform.getOrigin().x());
-		// above kinect is negative pitch
-		pitch = -atan2(transform.getOrigin().z(),
-					transform.getOrigin().x());
-		std::cout << "Got transform, yaw: " << yaw
-				<< " pitch: " << pitch << std::endl;
-	} catch (tf::TransformException ex) {
-		ROS_ERROR("%s", ex.what());
-		ros::spinOnce();
-		ros::Duration(1.0).sleep();
-		// try another user string
-		if (activeUser < 10) activeUser++;
-		else activeUser = 1;
-		lookupTransform();
+			std::cout << "Got transform, yaw: " << yaw << " pitch: " << pitch
+					<< std::endl;
+		} catch (tf::TransformException ex) {
+			ROS_ERROR("%s", ex.what());
+			ros::spinOnce();
+			ros::Duration(1.0).sleep();
+			// try another user string
+			if (activeUser < 10)
+				activeUser++;
+			else
+				activeUser = 1;
+			lookupTransform();
 
-	}
+		}
+	} else
+		return;
 }
